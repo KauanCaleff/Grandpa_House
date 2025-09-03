666325c9f6621d1955cffe689bb646dd15ce2fe1
marcellamanu <emanuelemarcarraro@gmail.com>
2025-09-03 10:54:18 -0300

    playeeer

---
+++ Grandpa House/EscapeRoom Unity/Assets/Script/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController character;
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        //float x = Input.GetAxis("Horizontal");
+        //float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput;
+
+        character.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        character.Move(velocity * Time.deltaTime);
+
+    }
+
+    public void OnMoveEvent(InputAction.CallbackContext value)
+    {
+        horizontalInput = value.ReadValue<Vector2>().x;
+        verticalInput = value.ReadValue<Vector2>().y;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+}

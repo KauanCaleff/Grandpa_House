61e4d4a54a9500ea7b8e174f75cbf2ada417b43d
mihlwz <20233023974@estudantes.ifpr.edu.br>
2025-09-10 11:22:21 -0300

    keypad

---
+++ Grandpa House/EscapeRoom Unity/Assets/Script/Keypad.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Keypad : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI Ans;
+    private string answer = "1412";
+
+    public void Number(int number)
+    {
+        Ans.text += number.ToString();
+    }
+
+    public void Enter()
+    {
+        if(Ans.text == answer){
+            Ans.text = "CERTOU";
+        }else{
+            Ans.text = "ERROR  ";
+        }
+    }
+}

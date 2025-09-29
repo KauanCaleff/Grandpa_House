ff96ca721534c23ffdf363d335729a9e64d8a177
KauanCaleff <kauancalefi2007@gmail.com>
2025-09-29 09:40:12 -0300

    Mini Game Elétrico

---
+++ Grandpa House/EscapeRoom Unity/Assets/Script/MatchLogic.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchLogic : MonoBehaviour
+{
+    static MatchLogic Instance;
+
+    public int maxPoints=5;
+    public Text pointsText;
+    public GameObject levelCompletedUI;
+    private int points=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Instance = this; 
+    }
+
+    void UpdatePotinsText(){
+        pointsText.text = points + "/" + maxPoints;
+        if (points == maxPoints){
+            levelCompletedUI.SetActive(true);
+        }
+    }
+
+    public static void AddPoint(){
+        AddPoints(1);
+    }
+
+    public static void AddPoints(int points){
+        Instance.points += points;
+        Instance.UpdatePotinsText();
+    }
+}

8d1d0daa570de4b93dca5fcd9489db1e1065bdb4
mihlwz <20233023974@estudantes.ifpr.edu.br>
2025-09-29 08:49:05 -0300

    inventario

--- Grandpa House/EscapeRoom Unity/Assets/Script/Item.cs
+++ Grandpa House/EscapeRoom Unity/Assets/Script/Item.cs
@@ -8,4 +8,7 @@ public class Item : ScriptableObject
     public bool grabbable;
     //audio
     public string name;
+    [Header("Invetario")]
+    public bool InventarioItem;
+    public string CollectMessage;
 }
--- Grandpa House/EscapeRoom Unity/Assets/Script/Uimanager.cs
+++ Grandpa House/EscapeRoom Unity/Assets/Script/Uimanager.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class Uimanager : MonoBehaviour
 {
     public static Uimanager Instance;
     public GameObject handCursor;
+    public GameObject invetarioImage;
+    public TextMeshProUGUI[] invetarioItens;
+    public TextMeshProUGUI infoText;
+    private Inventario inventario;
+    public InputActionReference interactionInputAction;
 
     private void Awake()
     {
         Instance = this;
+        inventario = GetComponent<Inventario>();
+    }
+    
+
+    private void OnEnable()
+    {
+        interactionInputAction.action.performed += InventarioI;
+    }
+
+    private void OnDisable()
+    {
+        interactionInputAction.action.performed -= InventarioI;
     }
     // Start is called before the first frame update
     void Start()
@@ -23,6 +43,16 @@ public class Uimanager : MonoBehaviour
         
     }
 
+    public void InventarioI(InputAction.CallbackContext inv)
+    {
+        invetarioImage.SetActive(!invetarioImage.activeInHierarchy);
+    }
+    public void SetItens(Item item, int index)
+    {
+        invetarioItens[index].text = item.CollectMessage;
+        infoText.text = item.CollectMessage;
+        //StartCoroutine(FadingText());
+    }
     public void SetHandCursor(bool state)
     {
         handCursor.SetActive(state);

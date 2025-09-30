be676e24317daf7f34b1654355287718e53084c8
marcellamanu <emanuelemarcarraro@gmail.com>
2025-09-30 19:24:47 -0300

    lanterna e martelo funcionando corretamente
    FINALMENTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Grandpa House/EscapeRoom Unity/Assets/Script/Flashlight.cs
+++ Grandpa House/EscapeRoom Unity/Assets/Script/Flashlight.cs
@@ -16,11 +16,8 @@ public class Flashlight : MonoBehaviour, IInteractable
 
     public bool equipped;
 
-    private Vector3 initialPosition;
-
     void Start()
     {
-        initialPosition = transform.position;
         if(!equipped)
         {
             rb.isKinematic = false;
@@ -55,7 +52,7 @@ public class Flashlight : MonoBehaviour, IInteractable
         coll.isTrigger = true;
 
         transform.SetParent(camera);
-        transform.localPosition = new Vector3(0.598f, -0.188f, 0.653f); 
+        transform.localPosition = new Vector3(0.598f, -0.188f, 0.653f); // ajuste conforme necessário
         transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
     }
 

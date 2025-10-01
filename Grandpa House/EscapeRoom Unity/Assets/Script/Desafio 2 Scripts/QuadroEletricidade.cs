2da4d83e8d0367adcdb50ef6e723d2d749b20505
KauanCaleff <kauancalefi2007@gmail.com>
2025-10-01 00:19:59 -0300

    Umas Alteração ae

--- Grandpa House/EscapeRoom Unity/Assets/Script/Desafio 2 Scripts/QuadroEletricidade.cs
+++ Grandpa House/EscapeRoom Unity/Assets/Script/Desafio 2 Scripts/QuadroEletricidade.cs
@@ -7,7 +7,6 @@ public class QuadroEletricidade : MonoBehaviour, IInteractable
 {
     [SerializeField] private ScriptableObject scriptableObject;
     [SerializeField] private GameObject ImageKeypad;
-    private string answer = "1412";
 
     public void IInteract()
     {

0438737212acf233ba0ab4f89fd66fe0a4e0c47f
marcellamanu <emanuelemarcarraro@gmail.com>
2025-10-13 09:45:42 -0300

    Coisas

--- Grandpa House/EscapeRoom Unity/Assets/Script/Desafio 2 Scripts/MatchLogic.cs
+++ Grandpa House/EscapeRoom Unity/Assets/Script/Desafio 2 Scripts/MatchLogic.cs
@@ -11,7 +11,10 @@ public class MatchLogic : MonoBehaviour
     public Text pointsText;
     public GameObject levelCompletedUI;
     private int points=0;
-    
+
+    public Mouse playerMouse;
+
+    public GameObject minigameUI;    
     public GameObject luzes;
     // Start is called before the first frame update
     void Start()
@@ -34,5 +37,22 @@ public class MatchLogic : MonoBehaviour
     public static void AddPoints(int points){
         Instance.points += points;
         Instance.UpdatePotinsText();
+    }
+
+    public void OpenMinigame()
+    {
+        minigameUI.SetActive(true);
+
+        if (playerMouse != null)
+            playerMouse.LockMouse(); // trava a câmera e libera o cursor
+    }
+
+    // 🔌 Quando o jogador fecha o painel elétrico
+    public void CloseMinigame()
+    {
+        minigameUI.SetActive(false);
+
+        if (playerMouse != null)
+            playerMouse.UnlockMouse(); // destrava a câmera e bloqueia o cursor
     }
 }
